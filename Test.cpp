5dc41ab31f5b6e132b24edff944c3a520069f146
luthda <david.luthiger@hsr.ch>
2019-12-03 16:40:12 +0100

    final changes

--- Test.cpp
+++ Test.cpp
@@ -4,24 +4,77 @@
 #include "xml_listener.h"
 #include "cute_runner.h"
 
-#include <string>
-#include <stdexcept>
-
 void testIndexAccess() {
 	indexableSet<int, std::less<int>> indexSet{1, 2, 3};
-	auto indexVal = indexSet[0];
+	int indexVal = indexSet[0];
 	ASSERT_EQUAL(1, indexVal);
 }
 
 void testNegativeIndexAccess() {
 	indexableSet<int> indexSet{1, 3, 5};
-	auto indexVal = indexSet[-1];
+	int indexVal = indexSet[-1];
 	ASSERT_EQUAL(5, indexVal);
+}
+
+void testNegativeAtAccess() {
+	indexableSet<int> indexSet{1, 3, 5, 7};
+	int indexVal = indexSet.at(-1);
+	ASSERT_EQUAL(7, indexVal);
+}
+
+void testNegativeIndexAccess2() {
+	indexableSet<int> indexSet{1, 3, 5};
+	ASSERT_EQUAL(1, indexSet.at(-3));
 }
 
 void testOutOfBoundException() {
 	indexableSet<std::string> strSet{":D", ";D", "xD"};
 	ASSERT_THROWS(strSet[3], std::out_of_range);
+}
+
+void testFrontMethod() {
+	indexableSet<int> indexSet{1, 2, 3};
+	int indexVal = indexSet.front();
+	ASSERT_EQUAL(1, indexVal);
+}
+
+void testBackMethod() {
+	indexableSet<int> indexSet{1, 2, 3};
+	int indexVal = indexSet.back();
+	ASSERT_EQUAL(3, indexVal);
+}
+void testInitListCtor() {
+	indexableSet<std::string> strSet{":D", ";D", "xD", ":)", ";)"};
+	ASSERT_EQUAL(5, strSet.size());
+}
+
+void testEmptyMethodFromBase() {
+	indexableSet<int> indexSet{ };
+	ASSERT(indexSet.empty());
+}
+
+void testAccessEmptySet() {
+	indexableSet<int> indexSet{ };
+	ASSERT_THROWS(indexSet[0], std::out_of_range);
+	ASSERT_THROWS(indexSet.front(), std::out_of_range);
+}
+
+void testCopyCtor() {
+	indexableSet<std::string> strSet{":D", ";D", "xD", ":)", ";)"};
+	indexableSet<std::string> copySet{strSet};
+	ASSERT_EQUAL(copySet.size(), strSet.size());
+}
+
+void testGreaterComperator() {
+	indexableSet<int, std::greater<int>> intSet{1, 2, 4, 6, 99};
+	ASSERT_EQUAL(99, intSet.front());
+	ASSERT_EQUAL(1, intSet.back());
+}
+
+void testLessComperator() {
+	indexableSet<int, std::less<int>> intSet{1, 2, 4, 6, 99};
+	ASSERT_EQUAL(1, intSet.front());
+	ASSERT_EQUAL(99, intSet.back());
 }
 
 void thisIsATest() {
@@ -32,8 +85,18 @@ bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	s.push_back(CUTE(thisIsATest));
 	s.push_back(CUTE(testIndexAccess));
-	s.push_back(CUTE(testOutOfBoundException));
 	s.push_back(CUTE(testNegativeIndexAccess));
+	s.push_back(CUTE(testNegativeAtAccess));
+	s.push_back(CUTE(testNegativeIndexAccess2));
+	s.push_back(CUTE(testOutOfBoundException));
+	s.push_back(CUTE(testFrontMethod));
+	s.push_back(CUTE(testBackMethod));
+	s.push_back(CUTE(testInitListCtor));
+	s.push_back(CUTE(testEmptyMethodFromBase));
+	s.push_back(CUTE(testAccessEmptySet));
+	s.push_back(CUTE(testCopyCtor));
+	s.push_back(CUTE(testGreaterComperator));
+	s.push_back(CUTE(testLessComperator));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner = cute::makeRunner(lis, argc, argv);
--- indexableSet.h
+++ indexableSet.h
@@ -6,47 +6,50 @@
 #include <stdexcept>
 
 template <typename T, typename COMPARE=std::less<T>>
-class indexableSet : std::set<T, COMPARE> {
+class indexableSet : public std::set<T, COMPARE> {
 	using container = std::set<T, COMPARE>;
 	using container::container;
-	using size_type = typename container::size_type;
-	using reference = typename container::reference;
 	using const_reference = typename container::const_reference;
+
+	const_reference move(int index) const {
+		auto it{this->begin()};
+		auto pointer = std::next(it, index);
+		return *pointer;
+	}
+
+	void emptyCheck() const {
+		if (this->empty()) {
+			throw std::out_of_range{"empty set!!"};
+		}
+	}
 
 public:
 	const_reference at(int index) const {
-		int actualSize = this->size();
-		if (index < 0) {
-			index += actualSize;
-		}
-		if (index < actualSize) {
-			auto it{this->begin()};
-			std::advance(it, index);
-			return *it;
+		int signedSize = this->size();
+		if (index < signedSize && index >= -signedSize) {
+			if (index < 0) index += signedSize;
+			return move(index);
 		}
 		else {
 			throw std::out_of_range{"index not in set range!!!"};
 		}
-
 	}
 
 	const_reference operator[](int index) const {
 		return this->at(index);
 	}
 
-	reference front(int index) {
-		return this[0];
+	const_reference back() const {
+		emptyCheck();
+		return this->at(this->size() - 1);
 	}
 
-	reference back(int index) {
-		return this[this->size() - 1];
+	const_reference front() const {
+		emptyCheck();
+		return this->at(0);
 	}
-
-
-
-
-
 };
+
 
 
 
